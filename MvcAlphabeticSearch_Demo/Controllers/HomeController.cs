156a2c3b76d6d9773926de3a9b586e778310be13
Farhan Ahmed <farhan1086@gmail.com>
2019-10-20 18:06:50 +0530

    initial commit

---
+++ MvcAlphabeticSearch_Demo/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using MvcAlphabeticSearch_Demo.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MvcAlphabeticSearch_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly CustomerContext db = new CustomerContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetCustomer()
+        {
+            var customer = db.Customers.ToList();
+            return Json(new { data = customer }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
